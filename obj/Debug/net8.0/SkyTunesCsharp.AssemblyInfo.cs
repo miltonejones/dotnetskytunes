58d42dcedd774320cd42ad082c1be0a2f72e1d47
Milton Jones <miltonejones@gmail.com>
2025-11-02 08:45:01 -0500

    added playlist editing

--- obj/Debug/net8.0/SkyTunesCsharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/SkyTunesCsharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkyTunesCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2c23cf902ea5d129d5c9145ac91d10d0aae7d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkyTunesCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkyTunesCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ wwwroot/js/playlist.js
@@ -0,0 +1,104 @@
+const API_ENDPOINT = "https://u8m0btl997.execute-api.us-east-1.amazonaws.com";
+
+const getPlaylistGrid = async (page = 1) => {
+  const response = await fetch(
+    API_ENDPOINT + `/request/Title/ASC/${page}/playlist`
+  );
+  return await response.json();
+};
+
+const savePlaylist = async (json) => {
+  const requestOptions = {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(json),
+  };
+  const response = await fetch(`${API_ENDPOINT}/playlist`, requestOptions);
+  return await response.json();
+};
+
+const updateList = async (playlist) => {
+  const { related } = playlist;
+  const verb =
+    related.indexOf(editedTrack.fileKey) > -1 ? "removed from" : "added to";
+  const updated = {
+    ...playlist,
+    related:
+      related.indexOf(editedTrack.fileKey) > -1
+        ? related.filter((f) => f !== editedTrack.fileKey)
+        : related.concat(editedTrack.fileKey),
+  };
+
+  await savePlaylist(updated);
+  console.log({ editedTrack, related });
+  const messageBox = document.getElementById("toast-message");
+  messageBox.innerHTML = `"${editedTrack.title}" ${verb} "${playlist.Title}"`;
+  updatePlaylistIcons();
+  hidePlaylist();
+
+  const toast = new bootstrap.Toast(document.getElementById("liveToast"));
+  toast.show();
+};
+
+let currentPlayLists = [];
+let relatedTracks = [];
+
+function saveToPlaylist(listKey) {
+  const item = currentPlayLists.find((f) => f.listKey === listKey);
+  if (!item) return alert("Could not find playlist " + listKey);
+  updateList(item);
+}
+
+function updateDrawerIcons() {
+  getPlaylistGrid().then((data) => {
+    currentPlayLists = data.records;
+
+    document.querySelectorAll(".related").forEach((item) => {
+      const listKey = item.getAttribute("data-listkey");
+      const title = item.getAttribute("data-title");
+      const list = currentPlayLists.find(
+        (f) => f.listKey === listKey || f.Title === title
+      );
+      //   console.log({ list });
+      if (list) {
+        item.style.cursor = "pointer";
+        item.classList.remove("fa-solid");
+        item.classList.remove("fa-regular");
+        const ok =
+          list.related.indexOf(editedTrack.fileKey) > -1
+            ? "fa-solid"
+            : "fa-regular";
+
+        item.classList.add(ok);
+      } else {
+        item.classList.add("fa-regular");
+      }
+    });
+  });
+}
+
+function updatePlaylistIcons() {
+  getPlaylistGrid().then((data) => {
+    const relatedPlaylists = [];
+    if (!data || !data.records) return relatedPlaylists;
+    data.records.forEach((item) => {
+      if (item.related && Array.isArray(item.related)) {
+        relatedPlaylists.push(...item.related);
+      }
+    });
+    relatedTracks = relatedPlaylists;
+    currentPlayLists = data.records;
+
+    document.querySelectorAll(".relatable").forEach((item) => {
+      item.style.cursor = "pointer";
+      item.classList.remove("fa-solid");
+      item.classList.remove("fa-regular");
+      const key = item.getAttribute("data-file-key");
+      const ok = relatedTracks.indexOf(key) > -1 ? "fa-solid" : "fa-regular";
+
+      item.classList.add(ok);
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", updatePlaylistIcons);
